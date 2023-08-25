fae960fbe34289c9e6a1acc6e901270b7a53a0e8
Anton Brashkov <spone161@gmail.com>
2023-08-25 22:45:39 +0300

    styling about us

--- src/layout/section/aboutUs/AboutUs.tsx
+++ src/layout/section/aboutUs/AboutUs.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import {Container} from "../../../components/Container";
 import {Button} from "../../../components/Button";
 import {theme} from "../../../styles/Theme";
+import {Icon} from "../../../components/icon/Icon";
 
 export const AboutUs = () => {
     return (
@@ -10,6 +11,7 @@ export const AboutUs = () => {
             <Container>
                 <h2>Find out more about how we work</h2>
                 <Button color={theme.colors.fontDarkBg}>how we work</Button>
+                <Icon iconId={'how-we-work-desktop'} width={'434'} height={'250'}/>
             </Container>
             {/*p80-60*/}
         </StyledAboutUs>
@@ -17,5 +19,38 @@ export const AboutUs = () => {
 };
 
 const StyledAboutUs = styled.section`
+  margin-bottom: 150px;
   
+  ${Container} {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: ${theme.colors.primary};
+    padding: 63px 75px;
+
+    position: relative;
+    
+    
+    h2 {
+      font-size: 62px;
+      color: ${theme.colors.fontDarkBg};
+      flex-basis: 550px;
+      line-height: 1;
+    }
+    
+    ${Button} {
+      z-index: 1;
+      &:hover {
+        color: #2C2A2F;
+        background-color: ${theme.colors.fontDarkBg};
+      }
+    }
+
+    svg {
+      position: absolute;
+      
+      top: 0;
+      right: 0;
+    }
+  }
 `
